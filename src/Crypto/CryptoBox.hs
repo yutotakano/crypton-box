46e9dada8485007e1bf5f8d41aebad058ac7c574
Yuto Takano <moa17stock@gmail.com>
2022-07-09 18:21:04 +0100

    Implement crypto_box_beforenm and crypto_box_afternm

--- src/Crypto/CryptoBox.hs
+++ src/Crypto/CryptoBox.hs
@@ -9,19 +9,51 @@ import Data.ByteString qualified as B
 import Crypto.Cipher.XSalsa qualified as XSalsa
 import Crypto.MAC.Poly1305 qualified as Poly1305
 import Crypto.PubKey.Curve25519 qualified as X25519
+import Crypto.Error (CryptoFailable(..), maybeCryptoError, CryptoError(..))
+import Crypto.ECC qualified as ECC
+import Data.Maybe (fromJust)
+import Data.Data (Proxy(..))
 
 -- | Build a @crypto_box@ packet encrypting the specified content with a
 -- 192-bit nonce, receiver public key and sender private key.
 crypto_box
     :: B.ByteString
+    -- ^ Message to encrypt
     -> B.ByteString
+    -- ^ 192-bit nonce
     -> X25519.PublicKey
+    -- ^ Public Key
     -> X25519.SecretKey
+    -- ^ Private Key
+    -> CryptoFailable B.ByteString
+    -- ^ Ciphertext
+crypto_box content nonce pk sk = do
+    let zero = B.replicate 16 0
+    shared <- ECC.ecdh (Proxy :: Proxy ECC.Curve_X25519) sk pk
+    let (iv0, iv1) = B.splitAt 8 nonce
+    let state0 = XSalsa.initialize 20 shared (zero `B.append` iv0)
+    let state1 = XSalsa.derive state0 iv1
+    let (rs, state2) = XSalsa.generate state1 32
+    let (c, _) = XSalsa.combine state2 content
+    let tag = Poly1305.auth (rs :: B.ByteString) c
+    return $ BA.convert tag `B.append` c
+
+crypto_box_beforenm
+    :: X25519.PublicKey
+    -> X25519.SecretKey
+    -> CryptoFailable ECC.SharedSecret
+crypto_box_beforenm pk sk = do
+    let zero = B.replicate 16 0
+    ECC.ecdh (Proxy :: Proxy ECC.Curve_X25519) sk pk
+
+crypto_box_afternm
+    :: B.ByteString
     -> B.ByteString
-crypto_box content nonce pk sk = BA.convert tag `B.append` c
+    -> ECC.SharedSecret
+    -> B.ByteString
+crypto_box_afternm content nonce shared = BA.convert tag `B.append` c
   where
     zero         = B.replicate 16 0
-    shared       = X25519.dh pk sk
     (iv0, iv1)   = B.splitAt 8 nonce
     state0       = XSalsa.initialize 20 shared (zero `B.append` iv0)
     state1       = XSalsa.derive state0 iv1
